17ab1f89a1af33593a631db601111350e9dea500
PC3961 <cpranavss65@gmail.com>
2024-02-03 17:24:53 +0530

    Movies_app

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+
+import { useEffect } from "react";
+//b87f0dfd
+const API_URL= 'http://www.omdbapi.com/apikey.aspx?VERIFYKEY=3df7a2b3-118b-4ddd-83df-521985adbaca'
+const App=()=>{
+  const  searchMovies = async(title)=>{
+    const response = await(`${API_URL}&s=${title}`);
+    const data= await response.json();
+    console.log(data.Search);
+  }
+  
+  useEffect(()=>{
+    searchMovies('title')
+  }, []);
+  return(
+  
+  <h1>App</h1>
+)
+}
+
+export default App;
